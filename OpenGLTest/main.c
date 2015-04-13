91c9a1e5c0052c4c3209e5d214c012f734811f21
hirano_tomoya <cromteria@gmail.com>
2015-04-14 07:06:15 +0900

    Initial Commit

---
+++ OpenGLTest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OpenGLTest
+//
+//  Created by Tomoya_Hirano on 2015/04/14.
+//  Copyright (c) 2015年 Tomoya_Hirano. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
