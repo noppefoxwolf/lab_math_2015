d0e0adbfdb4bf4069d6c0a0f59ccc752e886f878
hirano_tomoya <cromteria@gmail.com>
2015-11-17 15:22:22 +0900

    init

---
+++ OpenGLTest/file0616.c
@@ -0,0 +1,281 @@
+//
+//  file0602.c
+//  OpenGLTest
+//
+//  Created by Tomoya_Hirano on 6/2/15.
+//  Copyright (c) 2015 Tomoya_Hirano. All rights reserved.
+//
+
+//#include <string.h>
+//#include <math.h>
+#include "myglini.h"
+//#define N 8
+//#define RHO 0.4
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define imax 50							// こうして#defineであらかじめ定義した文字は、以降の変数宣言時にこれらを使って値を代入しながら宣言できる？
+#define jmax 50
+
+double u[imax+1][jmax+1];					// この配列宣言を、main関数の中で変数宣言同様の位置に書くと上手くいかないかもしれない？
+double uu[imax+1][jmax+1];					// uuはuの時間的に一個手前のものとしてコピーするもの
+double v[imax+1][jmax+1];
+double vv[imax+1][jmax+1];
+
+double P[imax+1][jmax+1];
+double T[imax+1][jmax+1];
+double TT[imax+1][jmax+1];
+
+
+/**draw関数*/
+void showData(int fileNo){
+    char filename[10] = {'\0'};
+    snprintf(filename, 10, "%.d.txt", fileNo);
+    FILE *file;
+    if((file=fopen(filename,"r"))!=NULL){
+        double buf[6];
+        //ファイルが終わるまで読み込む
+        int index = 0;
+        double dt = 0.2;
+        while( fscanf(file,"%lf,%lf,%lf,%lf,%lf,%lf,",&buf[0],&buf[1],&buf[2],&buf[3],&buf[4],&buf[5]) != EOF ){
+            index++;
+            //if (index%4==0){//間引き
+                //myline(buf[1],buf[2], buf[1]+(dt*buf[3]),buf[2]+(dt*buf[4]),  1, 0, 0, 1);
+                powLine(buf[1],//x0
+                        buf[2],//y0
+                        buf[1]+(dt*buf[3]),//x1
+                        buf[2]+(dt*buf[4]));//y1
+            //}
+            for(int i=0;i<10 ;i++){
+                char disp[12] = {'\0'};
+                snprintf(disp, 12, "t = %.03f", buf[0]);
+                glRasterPos3f(0.05*i - 0.5,-0.6,0);
+                glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, disp[i]);
+            }
+        }
+        fclose(file);
+    }
+}
+
+
+void calcData(){
+    //レイノルズ数が大きいほど物質はさらさらする
+    //風邪が吹くと物体の反対側で渦が生じ
+    float Re, Ra, Pr;
+    int i, j, n, m, N;					// 型の宣言時には、変数演算を用いて値を代入しながら宣言することはできない
+    float dx, dy, dt;						// したがってdxを用意した後dtを用意するとき、float dt=0.05*dx;とせず、float dt; dt=0.05*dx;とする
+    double A, B;							// もしかしたら単純な数演算を用いて値を代入しながら宣言することはできるかも？つまり、int Re=10;は可能かも？
+    
+    //fp = fopen("oneper.txt", "w");			// この文は変数宣言の後に書くのが普通(もしかしたらFILE *fp;の直後でも大丈夫かもしれない)
+    // wはwrite、rならread、rwも可
+    // このファイルをpov-rayのフォルダに手動コピーしてあげよう
+    // プログラム上で日本語名フォルダを指定して移動させることはできないから
+    
+    Re = 1000;
+    Ra = 1000000.0;
+    Pr = 10.0;
+    dx = 1.0 / (float)imax;
+    dy = 1.0 / (float)jmax;
+    dt = 0.05 * dx;							// 毎回このdtずつ足していく、その行為がN回行われる、よって最終的なtは(N-1)*dt
+    
+    N = (int)(10 / dt);						// 今は、結局N=200*imax=20000。ここで、int/(int)floatとしてしまうと分母0でエラー。
+    
+    FILE *fp = fopen("test.csv", "w");
+    fprintf(fp, "t,x,y,u,v,P,val");
+    // u[i][j],v[i][j],P[i][j]の計算と書き込み、時刻n*dt=0(n=0)のとき
+    // 課題ノートの[1]
+    n = 0;
+    for(i = 0; i <= imax; i++){
+        for(j = 0; j <= jmax; j++){
+            if(j < jmax) u[i][j] = 0;
+            else u[i][j] = 1;
+            v[i][j] = 0;
+            P[i][j] = 0;
+            //fprintf(fp, "%f,%f,%f,%f,%f,%f,\n", n*dt,i*dx, j*dy, u[i][j], v[i][j], P[i][j]);		// 書き込み
+            // pov-rayで読むために、書き込む文章中の数値の全ての最後にカンマをつける
+            // (t,x,y,u,v,P)と書く
+        }
+    }
+    //printf("第%d回目(時刻%f)　完了\n", n+1, n*dt);
+    
+    
+    // u[i][j],v[i][j],P[i][j]の計算と書き込み、時刻n*dt(n=1,2,...,N-1)のとき
+    // 課題ノートの、[2]~[4]の繰り返し
+    for(n = 1; n < N; n++){
+        for(i = 0; i <= imax; i++){
+            for(j = 0; j <= jmax; j++){
+                uu[i][j] = u[i][j];			// 全ての[i][j]で、uをコピーしておく
+                vv[i][j] = v[i][j];
+            }
+        }
+        
+        for (i=2; i<= imax-2; i++) {
+            for (j=2; j<= jmax-2; j++) {
+                float ududx = u[i][j] * (-u[i+2][j]+8*(u[i+1][j]-u[i-1][j])+u[i-2][j])/(12*dx) + (fabs(u[i][j])*powf(dx, 3)/12.0)*(u[i+2][j]-4*u[i+1][j]+6*u[i][j]-4*u[i-1][j]+u[i-2][j])/powf(dx, 4);//P183の7.75
+                float vdudy = v[i][j] * (-u[i][j+2]+8*(u[i][j+1]-u[i][j-1])+u[i][j-2])/(12*dy) + (fabs(v[i][j])*powf(dy, 3)/12.0)*(u[i][j+2]-4*u[i][j+1]+6*u[i][j]-4*u[i][j-1]+u[i][j-2])/powf(dy, 4);
+                float dPdx = (P[i+1][j]-P[i-1][j])/(2.0*dx);
+                float dx2 = dx*dx;
+                float dy2 = dy*dy;
+                float d2udx2 = ( uu[i+1][j] - 2.0*uu[i][j] + uu[i-1][j] ) / dx2;
+                float d2udy2 = ( uu[i][j+1] - 2.0*uu[i][j] + uu[i][j-1] ) / dy2;
+                u[i][j] = uu[i][j] + dt*( -ududx -vdudy - dPdx + 1.0/Re*(d2udx2+d2udy2) );
+                
+                float udvdx = u[i][j] * (-v[i+2][j]+8*(v[i+1][j]-v[i-1][j])+v[i-2][j])/(12*dx) + (fabs(u[i][j])*powf(dx, 3)/12.0)*(v[i+2][j]-4*v[i+1][j]+6*v[i][j]-4*v[i-1][j]+v[i-2][j])/powf(dx, 4);//P183の7.75
+                float vdvdy = v[i][j] * (-v[i][j+2]+8*(v[i][j+1]-v[i][j-1])+v[i][j-2])/(12*dy) + (fabs(v[i][j])*powf(dy, 3)/12.0)*(v[i][j+2]-4*v[i][j+1]+6*v[i][j]-4*v[i][j-1]+v[i][j-2])/powf(dy, 4);//P183の7.75
+                float dPdy = (P[i][j+1]-P[i][j-1])/(2.0*dy);
+                float d2vdx2 = ( uu[i+1][j] - 2.0*uu[i][j] + uu[i-1][j] ) / dx2;
+                float d2vdy2 = ( uu[i][j+1] - 2.0*uu[i][j] + uu[i][j-1] ) / dy2;
+                v[i][j] = vv[i][j] + dt*( -udvdx -vdvdy - dPdy + 1.0/Re*(d2vdx2+d2vdy2) );
+            }
+        }
+        
+        for (i=1; i< imax; i++) {
+            for (j=1; j< jmax; j+=jmax-2) {//上下の部分を計算
+                u[i][j] = uu[i][j]
+                + dt * ( - uu[i][j] * ( uu[i+1][j] - uu[i-1][j] ) / (2.0*dx)
+                        + fabs(uu[i][j]) / 2.0 * ( uu[i+1][j] - 2.0*uu[i][j] + uu[i-1][j] ) / dx
+                        - vv[i][j] * ( uu[i][j+1] - uu[i][j-1] ) / (2.0*dy)
+                        + fabs(vv[i][j]) / 2.0 * ( uu[i][j+1] - 2.0*uu[i][j] + uu[i][j-1] ) / dy
+                        - ( P[i+1][j] - P[i-1][j] ) / (2.0*dx)
+                        + 1.0/Re * ( ( uu[i+1][j] - 2.0*uu[i][j] + uu[i-1][j] ) / (dx*dx)
+                                    + ( uu[i][j+1] - 2.0*uu[i][j] + uu[i][j-1] ) / (dy*dy)
+                                    )
+                        );
+                v[i][j] = vv[i][j]
+                + dt * ( - uu[i][j] * ( vv[i+1][j] - vv[i-1][j] ) / (2.0*dx)
+                        + fabs(uu[i][j]) / 2.0 * ( vv[i+1][j] - 2.0*vv[i][j] + vv[i-1][j] ) / dx
+                        - vv[i][j] * ( vv[i][j+1] - vv[i][j-1] ) / (2.0*dy)
+                        + fabs(vv[i][j]) / 2.0 * ( vv[i][j+1] - 2.0*vv[i][j] + vv[i][j-1] ) / dy
+                        - ( P[i][j+1] - P[i][j-1] ) / (2.0*dy)
+                        + 1.0/Re * ( ( vv[i+1][j] - 2.0*vv[i][j] + vv[i-1][j] ) / (dx*dx)
+                                    + ( vv[i][j+1] - 2.0*vv[i][j] + vv[i][j-1] ) / (dy*dy)
+                                    )
+                        );
+            }
+        }
+        
+        for (i=1;i<imax; i+=imax-2){
+            for (j=1; j<jmax; j++) {//横の部分を計算
+                u[i][j] = uu[i][j]
+                + dt * ( - uu[i][j] * ( uu[i+1][j] - uu[i-1][j] ) / (2.0*dx)
+                        + fabs(uu[i][j]) / 2.0 * ( uu[i+1][j] - 2.0*uu[i][j] + uu[i-1][j] ) / dx
+                        - vv[i][j] * ( uu[i][j+1] - uu[i][j-1] ) / (2.0*dy)
+                        + fabs(vv[i][j]) / 2.0 * ( uu[i][j+1] - 2.0*uu[i][j] + uu[i][j-1] ) / dy
+                        - ( P[i+1][j] - P[i-1][j] ) / (2.0*dx)
+                        + 1.0/Re * ( ( uu[i+1][j] - 2.0*uu[i][j] + uu[i-1][j] ) / (dx*dx)
+                                    + ( uu[i][j+1] - 2.0*uu[i][j] + uu[i][j-1] ) / (dy*dy)
+                                    )
+                        );
+                v[i][j] = vv[i][j]
+                + dt * ( - uu[i][j] * ( vv[i+1][j] - vv[i-1][j] ) / (2.0*dx)
+                        + fabs(uu[i][j]) / 2.0 * ( vv[i+1][j] - 2.0*vv[i][j] + vv[i-1][j] ) / dx
+                        - vv[i][j] * ( vv[i][j+1] - vv[i][j-1] ) / (2.0*dy)
+                        + fabs(vv[i][j]) / 2.0 * ( vv[i][j+1] - 2.0*vv[i][j] + vv[i][j-1] ) / dy
+                        - ( P[i][j+1] - P[i][j-1] ) / (2.0*dy)
+                        + 1.0/Re * ( ( vv[i+1][j] - 2.0*vv[i][j] + vv[i-1][j] ) / (dx*dx)
+                                    + ( vv[i][j+1] - 2.0*vv[i][j] + vv[i][j-1] ) / (dy*dy)
+                                    )
+                        );
+            }
+        }
+        
+
+        for(m = 0; m < 100; m++){
+            for(i = 1; i < imax; i++){
+                for(j = 1; j < jmax; j++){
+                    A = - 1.0 * ( u[i+1][j] - u[i-1][j] ) / (2.0*dx) * ( u[i+1][j] - u[i-1][j] ) / (2.0*dx)
+                    - 1.0 * ( v[i][j+1] - v[i][j-1] ) / (2.0*dy) * ( v[i][j+1] - v[i][j-1] ) / (2.0*dy)
+                    - 2.0 * ( u[i][j+1] - u[i][j-1] ) / (2.0*dy) * ( v[i+1][j] - v[i-1][j] ) / (2.0*dx)
+                    + 1.0/dt * ( ( u[i+1][j] - u[i-1][j] ) / (2.0*dx) + ( v[i][j+1] - v[i][j-1] ) / (2.0*dy) );
+                    B = - ( P[i+1][j] + P[i-1][j] ) / (dx*dx) - ( P[i][j+1] + P[i][j-1] ) / (dy*dy) + A;
+                    P[i][j] = B / ( - 2.0 / (dx*dx) - 2.0 / (dy*dy) );
+                }
+            }
+        }
+        
+        // 課題ノートの[4]
+        for(j = 0; j <= jmax; j++){
+            P[0][j] = P[1][j];
+            P[imax][j] = P[imax-1][j];
+        }
+        for(i = 0; i <= imax; i++){
+            P[i][0] = P[i][1];
+            P[i][jmax] = P[i][jmax-1];
+        }
+        
+        // 時刻n*dt(第n回目時点)の書き込み
+        int flag = 0;
+        for(i = 0; i <= imax; i++){
+            for(j = 0; j <= jmax; j++){
+                if (flag == 0){
+                    fprintf(fp, "%f,%f,%f,%f,%f,%f,\n", n*dt,i*dx, j*dy, u[i][j], v[i][j], P[i][j]);
+                    flag = 1;
+                }if (flag == 1){
+                    flag = 2;
+                }else if (flag == 2){
+                    flag = 0;
+                }
+            }
+        }
+        
+        // 計算が長くなるため、コマンドプロンプトに進捗状況を表示させる
+        // 今回は全部で20000回
+        printf("%d/10000\n", n+1);
+//        printf("終了");
+    }
+    fclose(fp);
+    printf("終了");
+}
+
+void mydraw(){
+    //枠
+//    myline(0, 0, 0, 1, 1, 1, 1, 1);
+//    myline(0, 1, 1, 1, 1, 1, 1, 1);
+//    myline(1, 1, 1, 0, 1, 1, 1, 1);
+//    myline(1, 0, 0, 0, 1, 1, 1, 1);
+    calcData();
+//    showData(t);
+}
+
+//
+//void mydraw_xx(){
+//    double u[N+1], uu[N+1];				// N分割するから点はN+1個。uはn+1、uuはnのときのもの。
+//    float dx, dt;						// dx, dtをdoubleにしてしまうと、n>=1のときのu[j]の値がなぜかおかしくなる。
+//    int j, n;
+//    dx = 1.0 / (float)N / 2.0;				// 「x」は0～1。
+//    dt = dx * dx / 3.0 / 2.0;				// ρ=Δt/(Δx)^2=1/6にしたい
+//
+//    // 初期条件(すなわちn=0,t=0のとき。ここより、最初両端ではuは0になる。これをいじらなければずっと両端は0で出力される)
+//    for(j = 0; j <= N; j++){
+//        if(0 <= j && j <= N/2){
+//            u[j] = j * dx;			// 半分より左側
+//        }
+//        if(N/2 < j && j <= N ){
+//            u[j] = 1.0 - j * dx;		// 半分より右側
+//        }
+//        mypoint(j*dx, u[j], 1, 0, 0, 3);
+//        myline((j-1)*dx, u[j-1], (j)*dx, u[j], 1, 0, 0, 1);
+//    }
+//    
+//    // ここ以降で出力されるu[j]の値が、dx,dtがdoubleだとおかしくなってしまう。
+//    for(n = 1; n <= 1.0/dt; n++){		// dtは0未満の小数だから、1.0/dtは1より大きい
+//        for(j = 1; j < N; j++){
+//            uu[j] = u[j];				// u[j]の値をコピーしておく
+//        }
+//        for(j = 1; j < N; j++){			// 両端j=0,Nのときuは初期値0、これらはいじらなければ常に0
+//            u[j] = uu[j] + dt/(dx*dx) * (uu[j+1] - 2*uu[j] + uu[j-1]);		// dt/(dx*dx)は常に1.0/6.0だから、そう書いてもOK
+//            printf("%f\n",dt/(dx*dx));
+//            //u[j] = uu[j] + RHO * (uu[j+1] - 2*uu[j] + uu[j-1]);		// dt/(dx*dx)は常に1.0/6.0だから、そう書いてもOK
+//        }
+//        if(n%10 == 0){					// 出力される図がベタ塗りにならないように間引く
+//            for(j = 0; j <= N; j++){		// 両端j=0,Nのときも出力するため、出力のためのforを別個に作る
+//                printf("%f\n",u[j]);
+//                mypoint(j*dx, u[j], 1, 0, 0, 3);
+////                myline(j*dx, u[j], (j+1)*dx, u[j+1], 1, 0, 0, 1);
+//                myline((j-1)*dx, u[j-1], (j)*dx, u[j], 1, 0, 0, 1);
+//            }
+//        }
+//    }
+//}
--- OpenGLTest/myglini.h
+++ OpenGLTest/myglini.h
@@ -2,12 +2,19 @@
 //#include <GL/glut.h>
 //GLの互換性のあるGLUTを利用する
 #include <GLUT/glut.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include <math.h>
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"//deplicateワーニングを抑制
 
 void mydraw(void);
+void showData(int);
+GLint t = 0;
 
 void mypoint(float x, float y, float r, float g, float b, float s)
 {
+    x-=1;
+    y-=1;
     if(r > 1.0) r=1.0; if(g > 1.0) g=1.0; if(b > 1.0) b=1.0;
     if(r < 0.0) r=0.0; if(g < 0.0) g=0.0; if(b < 0.0) b=0.0;
     glColor3f(r,g,b);
@@ -20,6 +27,10 @@ void mypoint(float x, float y, float r, float g, float b, float s)
 void myline(float x0, float y0, float x1, float y1,
             float r, float g, float b, float s)
 {
+    x0-=1;
+    y0-=1;
+    x1-=1;
+    y1-=1;
     if(r > 1.0) r=1.0; if(g > 1.0) g=1.0; if(b > 1.0) b=1.0;
     if(r < 0.0) r=0.0; if(g < 0.0) g=0.0; if(b < 0.0) b=0.0;
     glColor3f(r,g,b);
@@ -28,6 +39,38 @@ void myline(float x0, float y0, float x1, float y1,
     glVertex2f(x0,y0);
     glVertex2f(x1,y1);
     glEnd();
+}
+
+//
+void powLine(float x0, float y0, float x1, float y1){
+
+    float width = x1 - x0;
+    float height = y1 - y0;
+    float  value = fabs(sqrt(width*width + height*height))*20.0;
+
+    int r, g, b;    // RGB値
+    double  tmp_val = cos( 4 * M_PI * value );
+    int     col_val = (int)( ( -tmp_val / 2 + 0.5 ) * 255 );
+    if ( value >= ( 4.0 / 4.0 ) ) { r = 255;     g = 0;       b = 0;       }   // 赤
+    else if ( value >= ( 3.0 / 4.0 ) ) { r = 255;     g = col_val; b = 0;       }   // 黄～赤
+    else if ( value >= ( 2.0 / 4.0 ) ) { r = col_val; g = 255;     b = 0;       }   // 緑～黄
+    else if ( value >= ( 1.0 / 4.0 ) ) { r = 0;       g = 255;     b = col_val; }   // 水～緑
+    else if ( value >= ( 0.0 / 4.0 ) ) { r = 0;       g = col_val; b = 255;     }   // 青～水
+    else {                               r = 0;       g = 0;       b = 255;     }   // 青
+    
+    
+    int isLine = 1;
+    if (isLine) {
+        myline(x0*2.0,
+               y0*2.0,
+               x1*2.0,
+               y1*2.0,
+               r/255.0, g/255.0, b/255.0, 1.7);
+        mypoint(x1*2.0, y1*2.0, r/255.0, g/255.0, b/255.0, 5.0);
+    }else{
+//        mypoint(x0*2.0, y0*2.0, value, 0, 1.0-value, 20.0);
+        mypoint(x0*2.0, y0*2.0, r/255.0, g/255.0, b/255.0, 20.0);
+    }
 }
 
 void mypolygon(int n, float *x0, float *y0, float r, float g, float b)
@@ -64,26 +107,43 @@ void mylineloop(int n, float *x0, float *y0,
     glEnd();
 }
 
-void display()
-{
+void display(){
     glClear(GL_COLOR_BUFFER_BIT);
     mydraw();
     glFlush();
 }
 
-void init()
-{
+void init(){
     glClearColor(0.0, 0.0, 0.0, 0.0);
+}
+
+/* 100ミリ秒ごとに実行される関数 */
+void timer(int value) {
+    /* 正方形のサイズを増加 */
+    t += 50;
+    if (t>5000){
+        t = 0;
+    }
+    //showData(t);
+        
+    /* 画面を再描写 */
+    glutPostRedisplay();
+    /* 100ミリ秒後に再実行 */
+    glutTimerFunc(100, timer, 0);
 }
 
 int main(int argc, char** argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition(0, 0);
-    glutCreateWindow("[-1,1] X [-1,1]     (500 X 500 pixel)");
-    glutDisplayFunc(display);
-    init();
-    glutMainLoop();
+    mydraw();
+    //    glutInit(&argc, argv);
+//    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+//    glutInitWindowSize(800, 800);
+//    glutInitWindowPosition(0, 100);
+//    glutCreateWindow("[-1,1] X [-1,1]");
+//    glutDisplayFunc(display);
+//    
+//    //glutTimerFunc(50, timer, 0);
+//    init();
+//    glutMainLoop();
+    
 }
